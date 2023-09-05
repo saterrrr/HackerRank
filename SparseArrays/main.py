142aeb72f57bf9ca3131745caa84483a7dcd2a16
Bartosz Szablinski <bartoszszablinskibiz@gmail.com>
2023-09-05 23:22:10 +0200

    Added SparseArrays

---
+++ SparseArrays/main.py
@@ -0,0 +1,59 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY strings
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(strings, queries):
+    string_counts = {}  
+    for string in strings:
+        if string in string_counts:
+            string_counts[string] += 1
+        else:
+            string_counts[string] = 1
+
+    results = []
+    for query in queries:
+        if query in string_counts:
+            results.append(string_counts[query])
+        else:
+            results.append(0)
+
+    return results
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input().strip())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
