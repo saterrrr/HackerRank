d784acedaab140a3d7d93afcd7d6ed05d501f0f9
Bartosz Szablinski <bartoszszablinskibiz@gmail.com>
2023-09-18 22:04:07 +0200

    Added Pangrams

---
+++ Pangrams/main.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    unique_letters = set()
+
+    for char in s:
+        char_lower = char.lower()
+        
+        if char_lower.isalpha():
+            unique_letters.add(char_lower)
+
+    if len(unique_letters) == 26:
+        return "pangram"
+    else:
+        return "not pangram"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
